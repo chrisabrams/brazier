d8f08da0bfdcce636489221a3d15efa6079924dd
Chris Abrams <mail@chrisabrams.com>
2014-11-28 09:51:21 -0600

    Add index file.

---
+++ index.js
@@ -0,0 +1,7 @@
+var Brazier = {
+  Controller : require('./src/controller'),
+  Dispatcher : require('./src/dispatcher'),
+  Router     : require('./src/router')
+}
+
+module.exports = Brazier
---
+++ test/unit/index.js
@@ -0,0 +1,16 @@
+var Brazier = require('../../index')
+
+describe('Index', function() {
+
+  it('should initialize', function(done) {
+
+    expect(Brazier).to.be.an('object')
+    expect(Brazier.Controller).to.be.a('function')
+    expect(Brazier.Dispatcher).to.be.a('function')
+    expect(Brazier.Router).to.be.a('function')
+
+    done()
+
+  })
+
+})
