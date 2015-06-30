afba235cf88dbc1f00ed3ad1202af7f49fef747c
Chris Abrams <mail@chrisabrams.com>
2015-06-30 16:04:43 -0700

    Move helpers

--- src/util.js
+++ src/util.js
@@ -5,29 +5,85 @@ var fs         = require('fs'),
 
 var Util = {}
 
+Util.lowerCaseFirstLetter = (s) => s.charAt(0).toLowerCase() + s.slice(1)
+Util.singularCase         = (s) => s.substring(0, s.length - 1)
+Util.upperCaseFirstLetter = (s) => s.charAt(0).toUpperCase() + s.slice(1)
+
+/*
+TODO: This could definitely be better optimized
+*/
 Util.copyFiles = (options = {}) => {
 
-  var destCwd = options.destCwd,
-      files   = options.files,
-      srcCwd  = options.srcCwd
+  if(options instanceof Array) {
 
-  files.forEach( (item) => {
+    options.forEach( (item) => {
 
-    let destDir = item.dest.substring(0, item.dest.lastIndexOf('/'))
+      let destDir = item.dest.substring(0, item.dest.lastIndexOf('/'))
 
-    var template = fs.readFileSync(path.join(srcCwd, item.src), 'utf8')
+      var template = fs.readFileSync(item.src, 'utf8')
 
-    mkdirp(destDir)
+      /*
+      TODO: Ideally, only make the same path once.
+      */
+      mkdirp(destDir)
 
-    if(item.data) {
-      template = Handlebars.compile(template)
-      template = template(item.data)
-    }
+      if(item.data) {
+        template = Handlebars.compile(template)
+        template = template(item.data)
+      }
 
-    fs.writeFileSync(path.join(destCwd, item.dest), template, 'utf8')
+      fs.writeFileSync(item.dest, template, 'utf8')
 
-  })
+    })
+
+  }
+
+  else {
+
+    var destCwd = options.destCwd,
+        files   = options.files,
+        srcCwd  = options.srcCwd
+
+    files.forEach( (item) => {
+
+      let destDir = item.dest.substring(0, item.dest.lastIndexOf('/'))
+
+      var template = fs.readFileSync(path.join(srcCwd, item.src), 'utf8')
+
+      /*
+      TODO: Ideally, only make the same path once.
+      */
+      mkdirp(path.join(destCwd, destDir))
+
+      if(item.data) {
+        template = Handlebars.compile(template)
+        template = template(item.data)
+      }
+
+      fs.writeFileSync(path.join(destCwd, item.dest), template, 'utf8')
+
+    })
+
+  }
 
 }
+
+Handlebars.registerHelper('lowerCase', (s) => {
+
+  return Util.lowerCaseFirstLetter(s)
+
+})
+
+Handlebars.registerHelper('singularCase', (s) => {
+
+  return Util.singularCase(s)
+
+})
+
+Handlebars.registerHelper('upperCase', (s) => {
+
+  return Util.upperCaseFirstLetter(s)
+
+})
 
 module.exports = Util
